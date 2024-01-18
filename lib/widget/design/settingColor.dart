f05b56427808cc6fe6aa4bc4f10c9174ebe5ee34
donguk <dragon071@naver.com>
2024-01-18 20:01:48 +0900

    chore : remove non needed folders

--- lib/widget/design/settingColor.dart
+++ lib/widget/design/settingColor.dart
@@ -3,7 +3,10 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 //bg colors
-const Color colorMainBG_greedot = Color.fromARGB(255, 62, 62, 62);
+
+const Color colorMainBG_greedot =
+    Color(0xFFF8ECE0); //Color.fromARGB(255, 62, 62, 62);
+
 const Color colorNaviBar_greedot = Color.fromARGB(255, 49, 49, 49);
 
 //text colors
