df05c7da581e905e5f1b6cc5dcaec0cba63bcee2
jiwoo moon <mjw0111@ewhain.net>
2024-01-24 17:02:21 +0900

    ...  ################  # feature : 새로운 기능 추가  # fix : 버그 수정  docs : describe commit convention at    README.md  # test : 테스트 코드 추가  # refactor : 코드 리팩토링  style : add color (snackBar, appBar, filling)  # chore : 빌드 부분 혹은 패키지 매니저  수정사항  ################
     ################
     # 본문(추가 설명)을 아랫줄에 작성   (참고하면 좋을 자료 사용한 위젯)  (optional)
    
     ################
     # 꼬릿말(footer)을 아랫줄에 작성    (관련된 이슈 번호 추가)(optional)
     Fixes: issue #N
     ...

--- lib/widget/design/settingColor.dart
+++ lib/widget/design/settingColor.dart
@@ -3,7 +3,7 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 //bg colors
-const Color colorMainBG_greedot = Color.fromARGB(255, 62, 62, 62);
+const Color colorMainBG_greedot = Color(0xFFF8ECE0);
 const Color colorNaviBar_greedot = Color.fromARGB(255, 49, 49, 49);
 
 //text colors
@@ -11,3 +11,27 @@ const Color colorText_greedot = Colors.white;
 
 //button colors
 const Color colorBut_greedot = Color(0xFFFC951C);
+
+//extra
+const Color colorSnackBar_greedot = Color(0xFF86B404);
+const Color colorFilling_greedot = Color(0xFFFBF5EF);
+const Color colorAppbar_greedot = Color(0xFFFF8000);
+
+// // 
+// import 'dart:ui';
+//
+// import 'package:flutter/material.dart';
+//
+// //bg colors
+// const Color colorMainBG_greedot = Color(0xFFF8ECE0);
+// const Color  colorNaviBar_greedot = Color.fromARGB(255, 49, 49, 49);
+// const Color colorAppbar_greedot = Color(0xFFFF8000); // 진 오렌지
+// const Color colorSnackBar_greedot = Color(0xFF86B503);
+// const Color colorFilling_greedot = Color(0xFFFBF5EF);
+//
+// //text colors
+// const Color colorText_greedot = Colors.white;
+//
+// //button colors
+// const Color colorBut_greedot = Color(0xFFFC951C);
+
