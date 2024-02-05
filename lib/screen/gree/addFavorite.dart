28ecf1476b89acf9e08b35790864b90b93c50d44
LeeSeongWon <tjddnjs10617@naver.com>
2024-02-05 09:05:28 +0900

    edit import addfavorite

--- lib/screen/gree/addFavorite.dart
+++ lib/screen/gree/addFavorite.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import '../../widget/design/settingColor.dart'; // 필요한 경로를 확인하세요
 import 'package:projectfront/widget/design/basicButtons.dart';
-import '../screen/rigging/getImage.dart'; // 버튼 사용을 위해 넣음
+import '../gree/addFavorite.dart';
+import '../rigging/getImage.dart'; // 버튼 사용을 위해 넣음
 
 class FavoriteItemCard extends StatefulWidget {
   final String image;
