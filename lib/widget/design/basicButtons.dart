7f94f3e8772ba12e4c5dbe666d442895f43cdc84
rimgosu <120752098+rimgosu@users.noreply.github.com>
2024-02-22 10:31:38 +0900

    update: button icon add

--- lib/widget/design/basicButtons.dart
+++ lib/widget/design/basicButtons.dart
@@ -1,35 +1,33 @@
 import 'package:flutter/material.dart';
 import './settingColor.dart';
 
-double paddingForButtons = 10;
-
 class EleButton_greedot extends StatelessWidget {
   final Widget Function()? gotoScene;
   final Color textColor, bgColor;
   final double width, height, fontSize;
   final EdgeInsetsGeometry padding;
   final String buttonText;
-  final void Function()? additionalFunc; //nullable
+  final void Function()? additionalFunc; // nullable
+  final IconData? icon; // 아이콘 추가
   final bool isSmall;
 
-  //default value setting
-  const EleButton_greedot(
-      {this.width = 125,
-        this.height = 45,
-        this.bgColor = colorBut_greedot,
-        this.textColor = colorBig_But_greedot,
-        required this.buttonText,
-        this.fontSize = 16.0,
-        this.gotoScene,
-        this.padding = const EdgeInsets.symmetric(vertical: 0.2, horizontal: 0.3),
-        this.additionalFunc, //nullable
-        this.isSmall = false,
-        Key? key})
-      : super(key: key);
+  const EleButton_greedot({
+    this.width = 125,
+    this.height = 45,
+    this.bgColor = colorBut_greedot,
+    this.textColor = Colors.white,
+    required this.buttonText,
+    this.fontSize = 16.0,
+    this.gotoScene,
+    this.padding = const EdgeInsets.symmetric(vertical: 0.2, horizontal: 0.3),
+    this.additionalFunc, // nullable
+    this.isSmall = false,
+    this.icon, // 아이콘 추가
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // isSmall 플래그에 따라 버튼 크기 결정
     final double buttonWidth = isSmall ? 70 : width;
     final double buttonHeight = isSmall ? 15 : height;
     final double buttonFontSize = isSmall ? 11.0 : fontSize;
@@ -38,22 +36,24 @@ class EleButton_greedot extends StatelessWidget {
     return ElevatedButton(
       style: ButtonStyle(
         backgroundColor: MaterialStateProperty.all(bgColor),
-        minimumSize: MaterialStateProperty.all(Size(buttonWidth, buttonHeight)), // 여기를 수정
-        padding: MaterialStateProperty.all(isSmall ? EdgeInsets.zero : padding),
+        minimumSize: MaterialStateProperty.all(Size(buttonWidth, buttonHeight)),
+        padding: MaterialStateProperty.all(buttonPadding),
         textStyle: MaterialStateProperty.all(TextStyle(fontSize: buttonFontSize)),
       ),
-      child: Text(buttonText, style: TextStyle(color: textColor, fontSize: buttonFontSize)),
       onPressed: () {
-        if (additionalFunc != null) {
-          additionalFunc!(); // additionalFunc이 null이 아닐 때만 호출
-        }
+        additionalFunc?.call(); // additionalFunc 호출 방식 간소화
         if (gotoScene != null) {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => gotoScene!()),
-          );
+          Navigator.push(context, MaterialPageRoute(builder: (context) => gotoScene!()));
         }
       },
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          if (icon != null) Icon(icon, color: Colors.white, size: 24), // 아이콘 색상을 흰색으로 설정
+          if (icon != null) SizedBox(width: 8), // 아이콘과 텍스트 사이에 공간 추가
+          Text(buttonText, style: TextStyle(color: textColor, fontSize: buttonFontSize)),
+        ],
+      ),
     );
   }
 }
