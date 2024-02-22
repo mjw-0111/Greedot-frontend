5844da3511159e4350e4671fb1a2be69ff44a834
donguk <dragon071@naver.com>
2024-02-22 11:20:03 +0900

    feature : add logo

--- lib/screen/gree/addFavorite.dart
+++ lib/screen/gree/addFavorite.dart
@@ -91,8 +91,7 @@ class _FavoriteItemCardState extends State<FavoriteItemCard> {
                 ],
               ),
             ),
-          )
-
+          ),
           Positioned(
             top: 12,
             left: 8,
